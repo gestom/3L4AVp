98924e4970d3c65833caf2631b0e66c68e1a5fd5
Tom Krajnik <tkrajnik@labe.felk.cvut.cz>
2018-12-26 14:15:58 +0100

    Publishing negative and positive clusters

--- radar/src/radarology.cpp
+++ radar/src/radarology.cpp
@@ -15,17 +15,20 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <people_msgs/PositionMeasurementArray.h>
+#include <pcl/segmentation/conditional_euclidean_clustering.h>
 
 using namespace std;
 float radius=0.5;
 int minClusterSize = 10;
 ros::Subscriber radar_sub_;
 ros::Subscriber laser_sub_;
-ros::Publisher point_pub_;
+ros::Publisher point_positive_pub_;
+ros::Publisher point_negative_pub_;
 ros::Subscriber tracker_sub_;
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
-PointCloud::Ptr pcl_msg (new PointCloud);
+typedef pcl::PointXYZI PointTypeFull;
+typedef pcl::PointCloud<PointTypeFull> PointCloud;
 float personX,personY;
+PointCloud::Ptr pcl_msg (new PointCloud);
 
 void callback(radar::radiusConfig &config, uint32_t level) {
 	
@@ -76,11 +79,11 @@ void backupRadarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 		neighbourArray.push_back(neighbourSize);
 		if (neighbourSize > minClusterSize){ 
 //			printf("PUBLISHED %f %f %f\n",xi,yi,zi);
-			pcl_msg->points.push_back (pcl::PointXYZ(xi,yi,zi));
+//			pcl_msg->points.push_back (pcl::PointXYZ(xi,yi,zi));
 			pcl_msg->width++;
 		}
 	}
-	point_pub_.publish (pcl_msg);
+//	point_pub_.publish (pcl_msg);
 
 	//Print results
 	cout << "Number of neigbours of points are "; 
@@ -91,7 +94,7 @@ void backupRadarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 
 }
 
-void radarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+void trueRadarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
 	//Convert msg to pcl
 	cout << "New pcl received" << endl;
@@ -111,7 +114,7 @@ void radarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 	pcl_msg->width = 0;
 	pcl_msg->points.clear();
 
-	/* Find neighbours for poincloud points */
+	/* Find neighbours of people detections */
 	int neighbourSize=0;
 	float xi = personX;
 	float yi = personY;
@@ -125,11 +128,11 @@ void radarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 
 		//Point lies within defined radius
 		if(r2<(radius*radius)){
-			pcl_msg->points.push_back (pcl::PointXYZ(xj,yj,zj));
+//			pcl_msg->points.push_back (pcl::PointXYZ(xj,yj,zj));
 			pcl_msg->width++;
 		}
 	}
-	point_pub_.publish (pcl_msg);
+//	point_pub_.publish (pcl_msg);
 
 	//Print results
 	cout << "Number of neigbours of points are "; 
@@ -138,6 +141,74 @@ void radarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 	}*/
 	cout << endl;
 
+}
+
+
+bool distanceSimilarity (const PointTypeFull& point_a, const PointTypeFull& point_b, float squared_distance)
+{
+  if (sqrt((point_a.x - point_b.x)*(point_a.x - point_b.x)+(point_a.y - point_b.y)*(point_a.y - point_b.y)) < radius)
+    return (true);
+  else
+    return (false);
+}
+
+void allRadarCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+	//Convert msg to pcl
+	cout << "New pcl received" << endl;
+	pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_pc(new pcl::PointCloud<pcl::PointXYZI>);
+	pcl::fromROSMsg(*msg, *pcl_pc);
+	pcl::PointCloud<PointTypeFull>::Ptr cloud_with_normals (new pcl::PointCloud<PointTypeFull>);
+	pcl::IndicesClustersPtr clusters (new pcl::IndicesClusters);
+	pcl::PointCloud<PointTypeFull>::Ptr	cloud_out (new pcl::PointCloud<PointTypeFull>);
+	//pcl::copyPointCloud (*pcl_pc,*cloud_out);
+
+	//Set up pcl
+	pcl::IndicesPtr pc_indices(new std::vector<int>);
+	pcl::PassThrough<pcl::PointXYZI> pt;
+	pt.setInputCloud(pcl_pc);
+	pt.filter(*pc_indices);
+
+	//std::cerr << "Segmenting to clusters...\n", tt.tic ();
+	pcl::ConditionalEuclideanClustering<PointTypeFull> cec (true);
+	cec.setInputCloud (pcl_pc);
+
+	cec.setClusterTolerance (500);//TODO what is it?
+	cec.setConditionFunction (&distanceSimilarity);
+
+	//cec.setMaxClusterSize (cloud_with_normals->points.size () / 5);
+	cec.segment (*clusters);
+	//std::cerr << ">> Done: " << tt.toc () << " ms\n";
+	printf("Clusters: %i: ",clusters->size());
+
+	for (int i = 0; i < clusters->size (); ++i)
+	{
+		printf("%i ",((*clusters)[i]).indices.size());
+		float meanX=0;
+		float meanY=0;
+		for (int j = 0; j <((*clusters)[i]).indices.size(); ++j){
+			meanX += pcl_pc->points[((*clusters)[i]).indices[j]].x;
+			meanY += pcl_pc->points[((*clusters)[i]).indices[j]].y;
+		}
+		meanX = meanX/((*clusters)[i]).indices.size();
+		meanY = meanY/((*clusters)[i]).indices.size();
+		printf("%f, ",sqrt((meanX-personX)*(meanX-personX)+(meanY-personY)*(meanY-personY)));
+
+		float intensity = 0.1;
+		pcl_msg->header.frame_id = "laser";
+		pcl_msg->height = 1;
+		pcl_msg->width = 0;
+		pcl_msg->points.clear();
+		for (int j = 0; j <((*clusters)[i]).indices.size(); ++j){
+			//if (sqrt((meanX-personX)*(meanX-personX)+(meanY-personY)*(meanY-personY)) < 0.5) intensity = 1.0;
+			//cloud_out->points[(*clusters)[i].indices[j]].intensity = intensity;
+			pcl_msg->points.push_back (pcl_pc->points[(*clusters)[i].indices[j]]);
+			pcl_msg->width++;
+		}
+		if (sqrt((meanX-personX)*(meanX-personX)+(meanY-personY)*(meanY-personY)) > 0.5) point_negative_pub_.publish (pcl_msg); else point_positive_pub_.publish (pcl_msg);
+	}
+
+	printf("\n");
 }
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
@@ -165,10 +236,11 @@ int main(int argc, char **argv) {
   dynamic_reconfigure::Server<radar::radiusConfig>::CallbackType f = boost::bind(&callback, _1, _2);
   server.setCallback(f);
 
-  radar_sub_ = nh_.subscribe<sensor_msgs::PointCloud2>("/radar/RScan", 1, radarCallback);
+  radar_sub_ = nh_.subscribe<sensor_msgs::PointCloud2>("/radar/RScan", 1, allRadarCallback);
   laser_sub_ = nh_.subscribe<sensor_msgs::LaserScan>("/scan", 1, laserCallback);
   tracker_sub_ = nh_.subscribe<people_msgs::PositionMeasurementArray>("/people_tracker_measurements", 1, trackerCallback);
-  point_pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZ> > ("/points2", 100000);
+  point_positive_pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZ> > ("/positive", 100000);
+  point_negative_pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZ> > ("/negative", 100000);
 
   ros::spin();
   return 0;
