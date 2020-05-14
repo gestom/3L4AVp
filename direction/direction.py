2aa423ccf4ae31ef6d1aaf0513bb327bd6bca9c5
George Broughton <broughton_g182@hotmail.com>
2020-05-14 06:08:24 +0100

    magnitude based histogram

--- direction/direction.py
+++ direction/direction.py
@@ -14,8 +14,10 @@ pi = 3.14159265358979
 rad = 360/(2*pi)
 
 bearings = []
+mags = []
 
 with open("bearing.txt", "w") as f:
+    f.write("bearing magnitude")
     for line in data:
         line = line.split(" ")
         try:
@@ -28,7 +30,11 @@ with open("bearing.txt", "w") as f:
             bearing = math.atan2(x2 - x1, y1 - y2)
             bearing = -(bearing - pi) * rad
 
-            f.write(str(bearing) + "\n")
+            mag = (x2-x1)*(x2-x1)*(y2-y1)*(y2-y1)
+            mag = mag**0.5
+            mags.append(float(mag))
+
+            f.write(str(bearing) + " " + str(mag) + "\n")
             bearings.append(float(bearing))
 
         except:
@@ -37,7 +43,32 @@ with open("bearing.txt", "w") as f:
 
 print("bearings written to bearings.txt")
 
-bins = list(range(0, 360, 1))
-bins.append(360)
-plt.hist(bearings, bins=bins)
+
+#without taking into account magnitude:
+#bins = list(range(0, 360, 1))
+#bins.append(360)
+#plt.hist(bearings, bins=bins)
+#plt.show()
+
+#with taking into account:
+binSize = 4
+binBoundaries = list(range(0, 360, binSize))
+binEnds = list(range(binSize, 360 + binSize, binSize))
+bins = []
+for i in binBoundaries:
+    bins.append(0)
+
+for bearing in bearings:
+    for j in range(0, len(binBoundaries)):
+        if bearing > binBoundaries[j] and bearing < binEnds[j]:
+            bins[j] += mag
+
+binMids = []
+current = binSize / 2
+for i in range(0, len(binBoundaries)):
+    binMids.append(current)
+    current += binSize
+
+plt.scatter(binMids, bins)
 plt.show()
+
