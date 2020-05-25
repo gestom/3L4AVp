dfba89ef030b4c92e7d6e05f7f47fa00df15751b
George Broughton <broughton_g182@hotmail.com>
2020-05-25 09:41:40 +0100

    doppler correction

--- radar/src/dopler_correction.cpp
+++ radar/src/dopler_correction.cpp
@@ -22,22 +22,47 @@ using namespace std;
 using namespace message_filters;
 ros::Publisher radar_pub;
 
+float linearX = 0.0f;
+ros::Time ts;
 
-void callback(const nav_msgs::Odometry::ConstPtr& odom, const sensor_msgs::PointCloud2::ConstPtr& radar)
+void odomCallback(const nav_msgs::Odometry::ConstPtr& odom)
 {
+    linearX = odom->twist.twist.linear.x;
+    ts = odom->header.stamp;
+}
 
-  pcl::PointCloud<pcl::PointXYZHSV>::Ptr pcl_pc(new pcl::PointCloud<pcl::PointXYZHSV>);
+void radarCallback(const sensor_msgs::PointCloud2::ConstPtr& radar)
+{
+    ros::Duration diff = ros::Time::now() - ts;
+
+    cout<<"diff: "<<diff<<endl;
+
+    ros::Duration limit(0.2);
+
+    cout << diff << " x " << limit << endl;
+
+    if(diff > limit)
+    {
+        printf("rejecting odom, too old\n");
+        return;
+    }
+
+    pcl::PointCloud<pcl::PointXYZHSV>::Ptr pcl_pc(new pcl::PointCloud<pcl::PointXYZHSV>);
   pcl::fromROSMsg(*radar, *pcl_pc);
 
   for (int i = 0 ; i < pcl_pc->points.size(); i++){
-    pcl_pc->points[i].v -= odom->twist.twist.linear.x;
+    pcl_pc->points[i].v -= linearX;
   }
 
   sensor_msgs::PointCloud2 out ;
   pcl::toROSMsg(*pcl_pc, out);
 
   radar_pub.publish(out);
+
+
 }
+
+
 
 int main(int argc, char **argv)
 {
@@ -49,14 +74,19 @@ int main(int argc, char **argv)
   std::string  radar_out_topic;
 
   n.param<std::string>("odom_in",  odom_topic, "/husky_velocity_controller/odom");
-  n.param<std::string>("radar_in",radar_in_topic, "/radar/RScan/aligned");
+  n.param<std::string>("radar_in",radar_in_topic, "/radar/RScan");
   n.param<std::string>("radar_out",radar_out_topic, "/radar_fixed");
 
+  printf("starting...\n");
+  printf("%s\n", radar_in_topic);
+
   radar_pub = n.advertise<sensor_msgs::PointCloud2>(radar_out_topic,1);
-  message_filters::Subscriber<nav_msgs::Odometry> odom_sub(n, odom_topic, 1);
-  message_filters::Subscriber<sensor_msgs::PointCloud2> radar_sub(n, radar_in_topic, 1);
-  TimeSynchronizer<nav_msgs::Odometry, sensor_msgs::PointCloud2> sync(odom_sub, radar_sub, 10);
-  sync.registerCallback(boost::bind(&callback, _1, _2));
+    ros::Subscriber sub_odom = n.subscribe(odom_topic,1, odomCallback);
+    ros::Subscriber sub_radar = n.subscribe(radar_in_topic,1,radarCallback);
+
+    //time sync requires matching ts
+  /*message_filters::Subscriber<nav_msgs::Odometry> odom_sub(n, odom_topic, 1);
+  message_filters::Subscriber<sensor_msgs::PointCloud2> radar_sub(n, radar_in_topic, 1);*/
 
 	ros::spin();
 	return 0;
