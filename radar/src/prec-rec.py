ac66280cb0baa94b7e1eef15f5044a2f03156239
George Broughton <broughton_g182@hotmail.com>
2020-09-01 18:30:15 +0100

    miltiple prec-recs

--- radar/src/prec-rec.py
+++ radar/src/prec-rec.py
@@ -8,14 +8,16 @@ from geometry_msgs.msg import PoseArray
 
 latestGT = []
 
-results = {}
+resultsD = {}
+resultsS = {}
 threshs = []
 x = 0
 while x <= 1.01:
     threshs.append(x)
     x += 0.001
 for i in threshs:
-    results[i] = {"tp": 0, "fp": 0, "fn": 0}
+    resultsD[i] = {"tp": 0, "fp": 0, "fn": 0}
+    resultsS[i] = {"tp": 0, "fp": 0, "fn": 0}
 
 def GTcallback(msg):
     global latestGT
@@ -25,7 +27,7 @@ def euclid(x, y, xx, yy):
     return ((x - xx)**2 + (y - yy)**2)**0.5
 
 def Dcallback(msg):
-    global latestGT, results
+    global latestGT, resultsD
 
     minProb = 999999999
     maxProb = -999999999
@@ -50,17 +52,53 @@ def Dcallback(msg):
     fps = 0
     fns = 0
 
-#    for thresh in threshs:
-#        for gt in latestGT:
-#            gtp = gt.position
-#            fn = True
-#            for point in nPoints:
-#                d = euclid(point[0], point[1], gtp.x, gtp.y)
-#                if d < 0.5:
-#                    if point[2] > thresh:
-#                        fn = False
-#            if fn == True:
-#                results[thresh]["fn"] += 1
+    for thresh in threshs:
+        for point in nPoints:
+            if point[2] > thresh:
+                insideClust = False
+                for gt in latestGT:
+                    gtp = gt.position
+                    d = euclid(point[0], point[1], gtp.x, gtp.y)
+                    if d < 0.5:
+                        insideClust = True
+                if insideClust:
+                    resultsD[thresh]["tp"] += 1
+                else:
+                    resultsD[thresh]["fp"] += 1
+            else:
+                insideClust = False
+                for gt in latestGT:
+                    gtp = gt.position
+                    d = euclid(point[0], point[1], gtp.x, gtp.y)
+                    if d < 0.5:
+                        insideClust = True
+                if insideClust:
+                    resultsD[thresh]["fn"] += 1
+
+def Scallback(msg):
+    global latestGT, resultsS
+
+    minProb = 999999999
+    maxProb = -999999999
+    for point in msg.points:
+        if point.z < minProb:
+            minProb = point.z
+        if point.z > maxProb:
+            maxProb = point.z
+
+    #normalised points
+    nPoints = []
+    for point in msg.points:
+        normProb = point.z
+        if normProb < 0:
+            normProb = 0
+        elif normProb > 1:
+            normProb = 1
+        nPoints.append([point.x, point.y, normProb])
+
+    tps = 0
+    fps = 0
+    fns = 0
 
     for thresh in threshs:
         for point in nPoints:
@@ -72,9 +110,9 @@ def Dcallback(msg):
                     if d < 0.5:
                         insideClust = True
                 if insideClust:
-                    results[thresh]["tp"] += 1
+                    resultsS[thresh]["tp"] += 1
                 else:
-                    results[thresh]["fp"] += 1
+                    resultsS[thresh]["fp"] += 1
             else:
                 insideClust = False
                 for gt in latestGT:
@@ -83,18 +121,26 @@ def Dcallback(msg):
                     if d < 0.5:
                         insideClust = True
                 if insideClust:
-                    results[thresh]["fn"] += 1
+                    resultsS[thresh]["fn"] += 1
+
 
 def listener():
-    global results
+    global resultsD, resultsS
     rospy.init_node('prec-rec')
-    #rospy.Subscriber("/radar_detector_ol/markers_prob", msgTemplate.Marker, Dcallback)
+    rospy.Subscriber("/radar_detector_ol/markers_prob", msgTemplate.Marker, Scallback)
     rospy.Subscriber("/deep_radar/out/points_raw", msgTemplate.Marker, Dcallback)
     rospy.Subscriber("/person/ground_truth", PoseArray, GTcallback)
     rospy.spin()
-    results = sorted(results.items(), key=lambda x:x[0])
-    for i in results:
-        print(i)
+    resultsD = sorted(resultsD.items(), key=lambda x:x[0])
+    resultsS = sorted(resultsS.items(), key=lambda x:x[0])
+
+    with open("/home/george/tmp/d.txt", "w") as f:
+        for i in resultsD:
+            f.write(str(i) + "\n")
+
+    with open("/home/george/tmp/s.txt", "w") as f:
+        for i in resultsS:
+            f.write(str(i) + "\n")
 
 if __name__ == '__main__':
     listener()
