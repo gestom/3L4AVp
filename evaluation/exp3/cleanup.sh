2ba594c2a846ceb09208dab0797c65d7a1f63ee9
Tom Krajnik <tomas.krajnik@agents.fel.cvut.cz>
2020-09-03 21:29:01 +0200

    Evaluation experiment 3

---
+++ evaluation/exp3/cleanup.sh
@@ -0,0 +1,6 @@
+*.log
+*.tmp
+*.
+*.tm
+*.fig
+*.pdf
---
+++ evaluation/exp3/preprocess.sh
@@ -0,0 +1,22 @@
+function process()
+{
+	a=1
+	cut -f 1,3,4,5,6,7,8,9,10 -d ' '  $1.txt |sed -n $a,1000000000p >svm.tp		
+	cat $1.txt |awk '{print $1,$3,$4,$11,$12,$13,$8,$9,$10}'|sed -n $a,1000000000p >cnn.tp 
+
+	./compute cnn.tp cnn.tm 10000 300
+	./compute svm.tp svm.tm 10000 300
+
+	cat cnn.tm |grep SLW >cnn.tmp
+	cat svm.tm |grep SLW >svm.tmp
+
+	gnuplot $1.gnu >$1.fig
+	fig2dev -Lpdf $1.fig >$1.pdf
+
+	rm *.tmp
+	rm *.tm
+	rm *.tp
+}
+
+process first
+process second 
